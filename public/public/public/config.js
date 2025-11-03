71b913ffafc2b0fa640471f4e9c67bdc7e4a3a24
nnelson702 <nnelson@skyecos.com>
2025-11-02 22:34:44 -0800

    Create config.js

---
+++ public/public/public/config.js
@@ -0,0 +1,5 @@
+// PUBLIC config (safe to commit; anon key is meant to be public)
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://bvyrxqfffaxthrjfxjue.supabase.co",
+  SUPABASE_ANON_KEY: "REPLACE_WITH_YOUR_ANON_KEY"
+};
