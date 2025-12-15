3bac7058208e9cf5e742f58ad85ec66a3761771b
nnelson702 <nnelson@skyecos.com>
2025-12-15 09:44:45 -0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,3 +1,5 @@
-export const SUPABASE_URL = "https://bvyrxqfffaxthrjfxjue.supabase.co";
-export const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2eXJ4cWZmZmF4dGhyamZ4anVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxMDkwMjEsImV4cCI6MjA3NzY4NTAyMX0.BK3LvTsDdLgFn5qNFHQoa4MTkGIe5sNvmVaA8uujvnM";
+// PUBLIC config (safe to commit; anon key is meant to be public)
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://bvyrxqfffaxthrjfxjue.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2eXJ4cWZmZmF4dGhyamZ4anVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxMDkwMjEsImV4cCI6MjA3NzY4NTAyMX0.BK3LvTsDdLgFn5qNFHQoa4MTkGIe5sNvmVaA8uujvnM"
+};
