8009ac21812f5d23a274aa42a23aa29aba132dd5
nnelson702 <nnelson@skyecos.com>
2025-12-15 02:08:50 -0800

    Update insights.js

--- public/insights.js
+++ public/insights.js
@@ -1,1 +1,13 @@
+window.HubInsights = (function () {
+  async function init(host) {
+    host.innerHTML = `
+      <div class="card">
+        <div class="card-body">
+          <div class="muted">Insights module scaffolded. Next: actual vs goal, MTD pace, ATV.</div>
+        </div>
+      </div>
+    `;
+  }
+  return { init };
+})();
 
