121fe0b7eadce792c4aa94cf86d0ca65019f34ad
nnelson702 <nnelson@skyecos.com>
2025-12-15 02:09:50 -0800

    Update marketing.js

--- public/marketing.js
+++ public/marketing.js
@@ -1,1 +1,13 @@
+window.HubMarketing = (function () {
+  async function init(host) {
+    host.innerHTML = `
+      <div class="card">
+        <div class="card-body">
+          <div class="muted">Marketing & Training scaffolded. Next: upload, categorize, download tracking.</div>
+        </div>
+      </div>
+    `;
+  }
+  return { init };
+})();
 
