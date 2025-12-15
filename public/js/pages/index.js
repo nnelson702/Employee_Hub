6295e9f9c2fad8be8316a16a555ba72ed795886d
nnelson702 <nnelson@skyecos.com>
2025-12-15 09:08:43 -0800

    Create index.js

---
+++ public/js/pages/index.js
@@ -0,0 +1,17 @@
+import { GoalsAdminPage } from "./goalsAdmin.js";
+import { GoalsInsightsPage } from "./goalsInsights.js";
+import { AdminPanelPage } from "./adminPanel.js";
+import { CommsFeedPage } from "./commsFeed.js";
+import { TasksPage } from "./tasks.js";
+import { DeptWalksPage } from "./deptWalks.js";
+import { MarketingTrainingPage } from "./marketingTraining.js";
+
+export const pages = {
+  goals_admin: GoalsAdminPage,
+  goals_insights: GoalsInsightsPage,
+  admin_panel: AdminPanelPage,
+  comms_feed: CommsFeedPage,
+  tasks: TasksPage,
+  dept_walks: DeptWalksPage,
+  marketing_training: MarketingTrainingPage,
+};
