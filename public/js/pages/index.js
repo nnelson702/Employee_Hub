3bac15b4bcbc754293bb01acc0d2be47c4a556e9
nnelson702 <nnelson@skyecos.com>
2025-12-15 13:40:55 -0800

    Update index.js

--- public/js/pages/index.js
+++ public/js/pages/index.js
@@ -1,17 +1,20 @@
-import { GoalsAdminPage } from "./goalsAdmin.js";
-import { GoalsInsightsPage } from "./goalsInsights.js";
-import { AdminPanelPage } from "./adminPanel.js";
-import { CommsFeedPage } from "./commsFeed.js";
-import { TasksPage } from "./tasks.js";
-import { DeptWalksPage } from "./deptWalks.js";
-import { MarketingTrainingPage } from "./marketingTraining.js";
+// public/js/pages/index.js
+import { renderDashboard } from "./dashboard.js";
+import { renderGoalsAdmin } from "./goals_admin.js";
+import { renderGoalsInsights } from "./goals_insights.js";
+import { renderCommsFeed } from "./comms_feed.js";
+import { renderTasks } from "./tasks.js";
+import { renderDeptWalks } from "./dept_walks.js";
+import { renderMarketingTraining } from "./marketing_training.js";
+import { renderAdminPanel } from "./admin_panel.js";
 
-export const pages = {
-  goals_admin: GoalsAdminPage,
-  goals_insights: GoalsInsightsPage,
-  admin_panel: AdminPanelPage,
-  comms_feed: CommsFeedPage,
-  tasks: TasksPage,
-  dept_walks: DeptWalksPage,
-  marketing_training: MarketingTrainingPage,
+export const PAGES = {
+  dashboard: { title: "Dashboard", render: renderDashboard },
+  goals_admin: { title: "Goals Admin", render: renderGoalsAdmin },
+  goals_insights: { title: "Goals & Insights", render: renderGoalsInsights },
+  comms_feed: { title: "Comms Feed", render: renderCommsFeed },
+  tasks: { title: "Tasks", render: renderTasks },
+  dept_walks: { title: "Dept Walks", render: renderDeptWalks },
+  marketing_training: { title: "Marketing & Training", render: renderMarketingTraining },
+  admin_panel: { title: "Admin Panel", render: renderAdminPanel },
 };
