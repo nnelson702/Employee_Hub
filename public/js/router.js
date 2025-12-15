1e2ce43f49370fc2297f9ed5264c8b0c107d1a71
nnelson702 <nnelson@skyecos.com>
2025-12-15 09:06:28 -0800

    Update router.js

--- public/js/router.js
+++ public/js/router.js
@@ -1,1 +1,28 @@
+export function createRouter({ onRoute }) {
+  let started = false;
+
+  function getRoute() {
+    const h = location.hash || "#goals_admin";
+    return h.replace("#", "");
+  }
+
+  async function run() {
+    const key = getRoute();
+    await onRoute(key);
+  }
+
+  async function start() {
+    if (started) return;
+    started = true;
+
+    window.addEventListener("hashchange", () => run().catch(console.error));
+    await run();
+  }
+
+  function refresh() {
+    run().catch(console.error);
+  }
+
+  return { start, refresh };
+}
 
