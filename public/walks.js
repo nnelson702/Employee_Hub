8a93becefaf72c75afc6082cf0d5c3a3c34e831d
nnelson702 <nnelson@skyecos.com>
2025-12-15 02:09:29 -0800

    Update walks.js

--- public/walks.js
+++ public/walks.js
@@ -1,1 +1,13 @@
+window.HubWalks = (function () {
+  async function init(host) {
+    host.innerHTML = `
+      <div class="card">
+        <div class="card-body">
+          <div class="muted">Dept Walks scaffolded. Next: templates, execution flow, issues + image upload.</div>
+        </div>
+      </div>
+    `;
+  }
+  return { init };
+})();
 
