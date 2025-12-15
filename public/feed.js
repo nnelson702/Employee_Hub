b27dc2592d0cb20ab8f3b51d2e28834b212168f5
nnelson702 <nnelson@skyecos.com>
2025-12-15 02:09:07 -0800

    Update feed.js

--- public/feed.js
+++ public/feed.js
@@ -1,1 +1,13 @@
+window.HubFeed = (function () {
+  async function init(host) {
+    host.innerHTML = `
+      <div class="card">
+        <div class="card-body">
+          <div class="muted">Comms Feed scaffolded. Next: posts, targets, comments, reactions, read receipts.</div>
+        </div>
+      </div>
+    `;
+  }
+  return { init };
+})();
 
