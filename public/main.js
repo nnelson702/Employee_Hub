574d5c2af8e54433f8fe04741afede04c37cfbe3
nnelson702 <nnelson@skyecos.com>
2025-12-15 09:45:49 -0800

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,1 +1,10 @@
+import { initAuth } from "./js/auth.js";
+import { initRouter } from "./js/router.js";
+import { mountAdminPanel } from "./js/pages/adminPanel.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  initRouter();
+  mountAdminPanel(document.querySelector("#route-admin-panel"));
+  await initAuth();
+});
 
